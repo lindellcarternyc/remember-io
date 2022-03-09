b4020c9340f060861e4b852aa87a85e2b6ad40f0
lindellcarternyc <lindellcarternyc@gmail.com>
2022-03-09 12:11:18 -0500

    creates horizontal scroll component

---
+++ src/ui/horizontal-scroll.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const HorizontalScroll = styled.div`
+  padding: 20px;
+  width: 100vw;
+  box-sizing: border-box;
+
+  display: flex;
+  gap: 10px;
+  overflow-x: auto;
+`
+
+export default HorizontalScroll
