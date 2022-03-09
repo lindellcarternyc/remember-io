3933c5f0b5899394be680bb550893ec9e71e1835
lindellcarternyc <lindellcarternyc@gmail.com>
2022-03-09 12:14:35 -0500

    creates global styles for app container

--- src/ui/app-container.tsx
+++ src/ui/app-container.tsx
@@ -1,11 +1,30 @@
 import { ReactNode } from 'react'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import { Normalize } from 'styled-normalize'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background: #3179ba;
+    width: 100%;
+    overflow: hidden;
+    font-family: Arial, sans-serif;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+`
 
 const AppContainerWrapper = styled.div`
   background-color: #3179ba;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  overflow: hidden;
 `
 
 interface AppContainerProps {
@@ -16,6 +35,7 @@ const AppContainer = ({ children }: AppContainerProps): JSX.Element => {
   return (
     <> 
       <Normalize />
+      <GlobalStyles />
       <AppContainerWrapper>
         {children}
       </AppContainerWrapper>
