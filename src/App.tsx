0cf56a4d1f642e34ed9c14c72408f57541b5323b
lindellcarternyc <lindellcarternyc@gmail.com>
2022-03-09 12:26:06 -0500

    use hscroll and task lists to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 
-import { AppContainer } from './ui'
+import { AppContainer, HorizontalScroll } from './ui'
+import { TaskList } from './task-list'
+import { ITaskList } from './task-list/interfaces'
+
+const LISTS: ITaskList[] = [
+  {
+    id: '0',
+    title: 'List 1',
+    tasks: [
+      {
+        id: '00',
+        text: 'Task 1'
+      }, {
+        id: '01',
+        text: 'Task 2'
+      }
+    ]
+  }
+]
 
 const App = (): JSX.Element => {
   return (
-    <AppContainer>App</AppContainer>
+    <AppContainer>
+      <div>App</div>
+      <HorizontalScroll>
+        {LISTS.map(list => {
+          return <TaskList key={list.id} list={list} />
+        })}
+      </HorizontalScroll>
+    </AppContainer>
   )
 }
 
