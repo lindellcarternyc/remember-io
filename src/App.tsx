a778c0569d3bddf4af622447e86a90cd4f0fa144
lindellcarternyc <lindellcarternyc@gmail.com>
2022-03-10 14:19:39 -0500

    adds dummy data to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,8 @@ import React from 'react'
 
 import { AppContainer, HorizontalScroll } from './ui'
 import { TaskList } from './task-list'
-import { ITaskList } from './task-list/interfaces'
 
-const LISTS: ITaskList[] = [
-  {
-    id: '0',
-    title: 'List 1',
-    tasks: [
-      {
-        id: '00',
-        text: 'Task 1',
-        priority: 'HIGH'
-      }, {
-        id: '01',
-        text: 'Task 2',
-        priority: 'MEDIUM'
-      }, {
-        id: '02',
-        text: 'Task 3',
-        priority: 'LOW'
-      }, {
-        id: '03',
-        text: 'Task 4'
-      }
-    ]
-  }
-]
+import { LISTS } from './data/task-lists'
 
 const App = (): JSX.Element => {
   return (
