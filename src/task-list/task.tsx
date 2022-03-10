77b4a793efb592a6f4192e1110b0767b6f677572
lindellcarternyc <lindellcarternyc@gmail.com>
2022-03-10 14:15:40 -0500

    adds task data to task component

--- src/task-list/task.tsx
+++ src/task-list/task.tsx
@@ -1,10 +1,24 @@
 import styled from 'styled-components'
 
 import { ITask } from './interfaces'
-import TaskPriority from './task-priority'
 
-const TaskWrapper = styled.div`
-  background-color: white;
+interface TaskProps {
+  task: ITask
+}
+
+const TaskWrapper = styled.div<Pick<TaskProps['task'], 'priority'>>`
+  background-color: ${({ priority }) => {
+    switch (priority) {
+      case 'HIGH':
+        return 'red'
+      case 'MEDIUM':
+        return 'darkgray'
+      case 'LOW':
+        return 'green'
+      default:
+        return 'white'
+    }
+  }};
   box-shadow: #0e1824 0px 1px 0px 0px;
   padding: 0.5rem 1rem;
   border-radius: 4px;
@@ -14,17 +28,10 @@ const TaskWrapper = styled.div`
   gap: 0.5rem;
 `
 
-interface TaskProps {
-  task: ITask
-}
-
 const Task = ({ task }: TaskProps): JSX.Element => {
   return (
-    <TaskWrapper>
+    <TaskWrapper priority={task.priority}>
       {task.text}
-      {task.priority && (
-        <TaskPriority priority={task.priority} />
-      )}
     </TaskWrapper>
   )
 }
