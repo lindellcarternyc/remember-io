404a6ac0775321c9038a61e78afc994cfb2d5957
lindellcarternyc <lindellcarternyc@gmail.com>
2022-03-12 17:33:40 -0500

    adds new types

--- src/data/task-lists.ts
+++ src/data/task-lists.ts
@@ -1,39 +1,43 @@
 import { ITaskList } from '../task-list/interfaces'
 
-const LISTS: ITaskList[] = [
-  {
+const LISTS: { [id: string]: ITaskList } = {
+  '0': {
     id: '0',
     title: 'List 1',
-    tasks: [
-      {
+    tasks: {
+      '00': {
         id: '00',
         text: 'Task 1',
         priority: 'HIGH'
-      }, {
+      }, 
+      '01': {
         id: '01',
         text: 'Task 2',
         priority: 'MEDIUM'
-      }, {
+      },
+      '02': {
         id: '02',
         text: 'Task 3',
         priority: 'LOW'
-      }, {
+      }, 
+      '03': {
         id: '03',
         text: 'Task 4'
       }
-    ]
-  }, {
+    }
+  }, 
+  '1': {
     id: '1',
     title: 'List 2',
-    tasks: [
-      {
+    tasks: {
+      '10': {
         id: '10',
         text: 'Do some stuff',
         priority: 'HIGH'
       }
-    ]
+    }
   }
-]
+}
 
 export {
   LISTS
